bfff49ef8f6db702ac92224e49e130227e3b2c80
Jacob Prinz <jacobeliasprinz@gmail.com>
2025-11-25 11:27:40 -0500

    basic unification working

--- LeanLambdaLibMathlib/LambdaLib/unification.lean
+++ LeanLambdaLibMathlib/LambdaLib/unification.lean
@@ -1,4 +1,7 @@
+import Mathlib.Tactic.failIfNoProgress -- why is this not in lean's standard library?
+
 import LambdaLib.qterm
+import LambdaLib.unificationFacts
 
 open QuotTerm
 
@@ -6,43 +9,13 @@ open QuotTerm
 in this file, i'll define the actual unification tactic
 -/
 
-theorem lam_body_rw {t1 t2 s1 s2} : (lam s1 t1 = lam s2 t2) = (t1 = t2) := by
-  apply propext
-  apply Iff.intro
-  · apply lam_body
-  · intros
-    subst_vars
-    exact alpha
-
-theorem const_inj_rw {c1 c2} : (const c1 = const c2) = (c1 = c2) := by
-  apply propext
-  apply Iff.intro
-  · apply const_inj
-  · intros; subst_vars; rfl
-
-theorem var_inj_rw {i1 i2} : (var i1 = var i2) = (i1 = i2) := by
-  apply propext
-  apply Iff.intro
-  · apply var_inj
-  · intros; subst_vars; rfl
-
--- TODO: i think this and the next theorem will be redundant with the general neutral form cases?
-theorem var_not_const_rw {i c} : (var i = const c) = False := by
-  apply propext
-  apply Iff.intro
-  · apply var_not_const
-  · intros; contradiction
-
-theorem var_not_const_rw2 {i c} : (const c = var i) = False := by
-  rw (config := {occs := .pos [2]}) [propext (Iff.intro Eq.symm Eq.symm)]
-  apply var_not_const_rw
-
-macro "basic_lambda_solve" : tactic => `(tactic|
+macro "lambda_solve" : tactic => `(tactic|
   repeat ( first
     | simp [lift_app, lift_lam, lift_var, lift_const, subst_app, subst_lam, subst_var,
       liftLiftMulti, substLiftMulti, subst_const, liftMultiZero, beta,
       lam_body_rw, const_inj_rw, var_inj_rw, var_not_const_rw, var_not_const_rw2] at *
-    | subst_vars -- TODO: maybe only have this go on equations of type QTerm
+    | simp (disch := repeat constructor) [app_fact_rw, app_ne_const_rw, app_ne_var_rw] at *
+    | fail_if_no_progress subst_vars -- TODO: maybe only have this go on equations of type QTerm
   )
 )
 
@@ -50,13 +23,13 @@ macro "basic_lambda_solve" : tactic => `(tactic|
 example (t1 t2 : QTerm)
   (H : < (λ x. x) {t1} > = <λ x. x>)
   : <{t1} {t2}> = t2 := by
-  basic_lambda_solve
+  lambda_solve
 
 example (H : <λ x. A> = <λ x. B>) : False := by
-  basic_lambda_solve
+  lambda_solve
 
 example (H : <λ x. A> = <λ x. x>) : False := by
-  basic_lambda_solve
+  lambda_solve
 
 /-
 there are two more cases i need unification to solve
@@ -77,191 +50,11 @@ then it can simp by that
 see: https://lean-lang.org/theorem_proving_in_lean4/Type-Classes/#decidable-propositions
 -/
 
-open SynTerm
+#check app_ne_var
+#check app_ne_const
+#check app_fact
 
-inductive Neutral : Term → Prop where
-| const : ∀{c}, Neutral (Term.const c)
-| var : ∀{n}, Neutral (Term.var n)
-| app : ∀{t arg}, Neutral t → Neutral (Term.app t arg)
+-- so i need to make simp able to try to prove QNeutral using (repeat constructor) maybe?
 
-theorem stepNeutral {t1 t2} (step : (union Step StepEta) t1 t2)
-  (H : Neutral t1)
-  : Neutral t2 := by
-  cases step with
-  | r step => cases step with
-    | app1 step =>
-      cases H
-      constructor
-      apply (stepNeutral (union.r step))
-      assumption
-    | app2 _ =>
-      cases H
-      constructor
-      assumption
-    | beta =>
-      cases H with | app H => _
-      cases H
-    | lam _ => cases H
-  | s step => cases step with
-    | app1 step =>
-      cases H
-      constructor
-      apply (stepNeutral (union.s step))
-      assumption
-    | app2 _ =>
-      cases H
-      constructor
-      assumption
-    | _ => cases H
-
-theorem allstepNeutral {t1 t2} (step : AllStep t1 t2)
-  (H : Neutral t1)
-  : Neutral t2 := by
-  induction step with
-  | refl => assumption
-  | cons _ _ ih =>
-    apply ih
-    apply stepNeutral <;> assumption
-
-lemma appStep' {a b t} (step : (union Step StepEta) (Term.app a b) t) (neut : Neutral a)
-  : ∃ a' b', t = Term.app a' b' ∧ Neutral a' := by
-  cases step with | r step => _ | s step => _ <;> cases step <;> (try (cases neut; done))
-    <;> try grind
-  · refine ⟨_, _, rfl, ?_⟩
-    apply (stepNeutral _ neut)
-    apply union.r
-    assumption
-  · refine ⟨_, _, rfl, ?_⟩
-    apply (stepNeutral _ neut)
-    apply union.s
-    assumption
-
-theorem appStep {a b t} (step : AllStep (Term.app a b) t) (neut : Neutral a)
-  : ∃ a' b', t = Term.app a' b' := by
-  generalize eqn : (Term.app a b) = t1 at *
-  induction step generalizing a b with
-  | refl => grind
-  | cons s ss ih =>
-    subst_vars
-    have ⟨a', b', eq, neut'⟩ := appStep' s neut
-    subst_vars
-    apply (ih neut' rfl)
-
-inductive QNeutral : QTerm → Prop where
-| const : ∀{c}, QNeutral (const c)
-| var : ∀{n}, QNeutral (var n)
-| app : ∀{t arg}, QNeutral t → QNeutral (app t arg)
-
-theorem liftNeutralWithArity {t}
-  : Neutral t → QNeutral (Quotient.mk _ t) := by
-  intro H
-  induction H with
-  | @const c =>
-    have thing := @QNeutral.const c
-    unfold const at thing
-    assumption
-  | @var i =>
-    have thing := @QNeutral.var i
-    unfold var at thing
-    assumption
-  | @app a b qn =>
-    -- apply Quotient.ind _ a
-    have thing := @QNeutral.app ⟦a⟧ ⟦b⟧
-    simp [app] at thing
-    apply thing
-    assumption
-
-theorem lowerNeutralWithArity {q} (H : QNeutral q)
-  : ∃ t, ⟦t⟧ = q ∧ Neutral t := by
-  induction H with
-  | @const c =>
-    exists .const c
-    simp [const]
-    repeat' constructor
-  | @var i =>
-    exists .var i
-    simp [var]
-    repeat' constructor
-  | @app a b qn ih =>
-    rcases ih with ⟨t, rfl, neu⟩
-    apply Quotient.ind _ b
-    intro b
-    exists .app t b
-    simp [app]
-    constructor
-    assumption
-
--- i would need these three theorems to make the unification tactic work
-theorem app_ne_var {i a b} (qneut : QNeutral a) (eq : var i = app a b) : False := by
-  have ⟨t, eq2, neut⟩ := lowerNeutralWithArity qneut
-  subst_vars
-  revert eq
-  apply Quotient.ind _ b
-  intro b eq
-  simp [var, app] at eq
-  have ⟨t', l, r⟩ := Quotient.exact eq
-  rcases varStep l with rfl
-  rcases appStep r neut with ⟨_, _, eq⟩
-  cases eq
-
-theorem app_ne_const {c a b} (qneut : QNeutral a) (eq : const c = app a b) : False := by
-  have ⟨t, eq2, neut⟩ := lowerNeutralWithArity qneut
-  subst_vars
-  revert eq
-  apply Quotient.ind _ b
-  intro b eq
-  simp [const, app] at eq
-  have ⟨t', l, r⟩ := Quotient.exact eq
-  rcases constStep l with rfl
-  rcases appStep r neut with ⟨_, _, eq⟩
-  cases eq
-
-theorem app_fact {a b a' b'} (qneut : QNeutral a) (qneut' : QNeutral a')
-  (eq : app a b = app a' b') : a = a' /\ b = b' := by
-  have ⟨t, eq2, neut⟩ := lowerNeutralWithArity qneut
-  have ⟨t, eq2, neut'⟩ := lowerNeutralWithArity qneut'
-  subst_vars
-  revert eq
-  apply Quotient.ind _ b
-  apply Quotient.ind _ b'
-  intro b b' eq
-  simp [app] at eq
-  have ⟨t', l, r⟩ := Quotient.exact eq
-  rcases appStep l neut with ⟨_, _, eq⟩
-  rcases appStep r neut' with ⟨_, _, eq⟩
-  subst_vars
-  --
-  sorry
-
--- i need a theorem that says that 2 neutrals of different arities are unequal?
--- or maybe prove this first on SynTerm.Terms?
-theorem nwa_inj {t} {lhs1 lhs2} {a1 a2}
-  (H1 : QNeutralWithArity lhs1 a1 t)
-  (H2 : QNeutralWithArity lhs2 a2 t)
-  : a1 = a2 ∧ lhs1 = lhs2 := by
-  induction H1 with -- try simp
-  | @const c =>
-    --
-    generalize bla : (const c) = thing at *
-    cases H2
-    --
-    sorry
-  | var =>
-    --
-    sorry
-  | app _ _ =>
-    --
-    sorry
-  --
-  --
-  --
-
-theorem neutral_unequal {t1 t2} {lhs1 lhs2} {a1 a2}
-  (H1 : QNeutralWithArity lhs1 a1 t1)
-  (H2 : QNeutralWithArity lhs2 a2 t2)
-  (notsame : a1 ≠ a2 ∨ lhs1 ≠ lhs2)
-  : t1 ≠ t2 := by
-  --
-  --
-  --
-  sorry
+example (t1 t2 : QTerm) (H : <A {t1} > = <A {t2} >) : t1 = t2 := by
+  lambda_solve
---
+++ LeanLambdaLibMathlib/LambdaLib/unificationFacts.lean
@@ -0,0 +1,248 @@
+import LambdaLib.qterm
+
+open QuotTerm
+
+theorem lam_body_rw {t1 t2 s1 s2} : (lam s1 t1 = lam s2 t2) = (t1 = t2) := by
+  apply propext
+  apply Iff.intro
+  · apply lam_body
+  · intros
+    subst_vars
+    exact alpha
+
+theorem const_inj_rw {c1 c2} : (const c1 = const c2) = (c1 = c2) := by
+  apply propext
+  apply Iff.intro
+  · apply const_inj
+  · intros; subst_vars; rfl
+
+theorem var_inj_rw {i1 i2} : (var i1 = var i2) = (i1 = i2) := by
+  apply propext
+  apply Iff.intro
+  · apply var_inj
+  · intros; subst_vars; rfl
+
+theorem var_not_const_rw {i c} : (var i = const c) = False := by
+  apply propext
+  apply Iff.intro
+  · apply var_not_const
+  · intros; contradiction
+
+theorem var_not_const_rw2 {i c} : (const c = var i) = False := by
+  rw (config := {occs := .pos [2]}) [propext (Iff.intro Eq.symm Eq.symm)]
+  apply var_not_const_rw
+
+open SynTerm
+
+inductive Neutral : Term → Prop where
+| const : ∀{c}, Neutral (Term.const c)
+| var : ∀{n}, Neutral (Term.var n)
+| app : ∀{t arg}, Neutral t → Neutral (Term.app t arg)
+
+theorem stepNeutral {t1 t2} (step : (union Step StepEta) t1 t2)
+  (H : Neutral t1)
+  : Neutral t2 := by
+  cases step with
+  | r step => cases step with
+    | app1 step =>
+      cases H
+      constructor
+      apply (stepNeutral (union.r step))
+      assumption
+    | app2 _ =>
+      cases H
+      constructor
+      assumption
+    | beta =>
+      cases H with | app H => _
+      cases H
+    | lam _ => cases H
+  | s step => cases step with
+    | app1 step =>
+      cases H
+      constructor
+      apply (stepNeutral (union.s step))
+      assumption
+    | app2 _ =>
+      cases H
+      constructor
+      assumption
+    | _ => cases H
+
+theorem allstepNeutral {t1 t2} (step : AllStep t1 t2)
+  (H : Neutral t1)
+  : Neutral t2 := by
+  induction step with
+  | refl => assumption
+  | cons _ _ ih =>
+    apply ih
+    apply stepNeutral <;> assumption
+
+def SmallStep := closeRef (union Step StepEta)
+
+lemma appStep' {a b t} (step : (union Step StepEta) (Term.app a b) t) (neut : Neutral a)
+  : ∃ a' b', t = Term.app a' b' ∧ SmallStep a a' ∧ SmallStep b b' /-Neutral a'-/ := by
+  cases step with | r step => _ | s step => _ <;> cases step <;> (try (cases neut; done))
+  · refine ⟨_, _, rfl, ?_⟩
+    apply And.intro
+    · apply Or.inr
+      apply union.r
+      assumption
+    · apply Or.inl
+      rfl
+  · refine ⟨_, _, rfl, ?_⟩
+    apply And.intro
+    · apply Or.inl
+      rfl
+    · apply Or.inr
+      apply union.r
+      assumption
+  · refine ⟨_, _, rfl, ?_⟩
+    apply And.intro
+    · apply Or.inr
+      apply union.s
+      assumption
+    · apply Or.inl
+      rfl
+  · refine ⟨_, _, rfl, ?_⟩
+    apply And.intro
+    · apply Or.inl
+      rfl
+    · apply Or.inr
+      apply union.s
+      assumption
+
+theorem appStep {a b t} (step : AllStep (Term.app a b) t) (neut : Neutral a)
+  : ∃ a' b', t = Term.app a' b' ∧ AllStep a a' ∧ AllStep b b' := by
+  generalize eqn : (Term.app a b) = t1 at *
+  induction step generalizing a b with
+  | refl =>
+    subst_vars
+    exists a, b
+    exact ⟨rfl, closure.refl, closure.refl⟩
+  | cons s ss ih =>
+    subst_vars
+    have ⟨a', b', eq, stepa, stepb⟩ := appStep' s neut
+    have neut' := allstepNeutral (closeRefToClosure stepa) neut
+    subst_vars
+    -- have test := (ih neut' rfl)
+    have ⟨ a'', b'', eq, ssa, ssb⟩  := ih neut' rfl
+    subst_vars
+    exists a'', b''
+    apply And.intro
+    · rfl
+    · apply And.intro
+      · exact (transitivity (closeRefToClosure stepa) ssa)
+      · exact (transitivity (closeRefToClosure stepb) ssb)
+
+inductive QNeutral : QTerm → Prop where
+| const : ∀{c}, QNeutral (const c)
+| var : ∀{n}, QNeutral (var n)
+| app : ∀{t arg}, QNeutral t → QNeutral (app t arg)
+
+theorem liftNeutralWithArity {t}
+  : Neutral t → QNeutral (Quotient.mk _ t) := by
+  intro H
+  induction H with
+  | @const c =>
+    have thing := @QNeutral.const c
+    unfold const at thing
+    assumption
+  | @var i =>
+    have thing := @QNeutral.var i
+    unfold var at thing
+    assumption
+  | @app a b qn =>
+    -- apply Quotient.ind _ a
+    have thing := @QNeutral.app ⟦a⟧ ⟦b⟧
+    simp [app] at thing
+    apply thing
+    assumption
+
+theorem lowerNeutralWithArity {q} (H : QNeutral q)
+  : ∃ t, ⟦t⟧ = q ∧ Neutral t := by
+  induction H with
+  | @const c =>
+    exists .const c
+    simp [const]
+    repeat' constructor
+  | @var i =>
+    exists .var i
+    simp [var]
+    repeat' constructor
+  | @app a b qn ih =>
+    rcases ih with ⟨t, rfl, neu⟩
+    apply Quotient.ind _ b
+    intro b
+    exists .app t b
+    simp [app]
+    constructor
+    assumption
+
+theorem app_ne_var {i a b} (qneut : QNeutral a) (eq : var i = app a b) : False := by
+  have ⟨t, eq2, neut⟩ := lowerNeutralWithArity qneut
+  subst_vars
+  revert eq
+  apply Quotient.ind _ b
+  intro b eq
+  simp [var, app] at eq
+  have ⟨t', l, r⟩ := Quotient.exact eq
+  rcases varStep l with rfl
+  rcases appStep r neut with ⟨_, _, eq⟩
+  rcases eq with ⟨eq, _, _⟩
+  cases eq
+
+theorem app_ne_const {c a b} (qneut : QNeutral a) (eq : const c = app a b) : False := by
+  have ⟨t, eq2, neut⟩ := lowerNeutralWithArity qneut
+  subst_vars
+  revert eq
+  apply Quotient.ind _ b
+  intro b eq
+  simp [const, app] at eq
+  have ⟨t', l, r⟩ := Quotient.exact eq
+  rcases constStep l with rfl
+  rcases appStep r neut with ⟨_, _, eq⟩
+  rcases eq with ⟨eq, _, _⟩
+  cases eq
+
+theorem app_fact {a b a' b'} (qneut : QNeutral a) (qneut' : QNeutral a')
+  (eq : app a b = app a' b') : a = a' /\ b = b' := by
+  have ⟨t, eq2, neut⟩ := lowerNeutralWithArity qneut
+  have ⟨t, eq2, neut'⟩ := lowerNeutralWithArity qneut'
+  subst_vars
+  revert eq
+  apply Quotient.ind _ b
+  apply Quotient.ind _ b'
+  intro b b' eq
+  simp [app] at eq
+  have ⟨t', l, r⟩ := Quotient.exact eq
+  rcases appStep l neut with ⟨_, _, eq, x, y⟩
+  rcases appStep r neut' with ⟨_, _, eq, z, w⟩
+  subst_vars
+  cases eq
+  apply And.intro <;> apply Quotient.sound
+  · refine ⟨_, x, z⟩
+  · refine ⟨_, y, w⟩
+
+theorem app_ne_var_rw {i a b} (qneut : QNeutral a) : (var i = app a b) = False := by
+  apply propext
+  apply Iff.intro
+  · apply app_ne_var qneut
+  · intros
+    contradiction
+
+theorem app_ne_const_rw {c a b} (qneut : QNeutral a) : (const c = app a b) = False := by
+  apply propext
+  apply Iff.intro
+  · apply app_ne_const qneut
+  · intros
+    contradiction
+
+theorem app_fact_rw {a b a' b'} (qneut : QNeutral a) (qneut' : QNeutral a')
+  : (app a b = app a' b') = (a = a' /\ b = b') := by
+  apply propext
+  apply Iff.intro
+  · apply app_fact qneut qneut'
+  · intro ⟨eq1, eq2⟩
+    subst_vars
+    rfl
